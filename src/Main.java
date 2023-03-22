7373a2b460487bd3c26546c129551d7fb748580d
Professional <new99kpicem@gmail.com>
2023-03-23 02:20:43 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,31 +21,26 @@ public class Main {
         array[5] = "Длинный";
         array[6] = "Очень";
         array[7] = "Длинный";
-        System.out.println(Arrays.toString(array));
-        String arrayNew = "";
-        for (int n = 0; n < array.length-1; n++) {
+        StringBuilder arrayNew = new StringBuilder();
+        for (int n = 0; n < array.length; n++) {
             for (int i = n+1; i < array.length; i++) {
                 boolean check = array[n].equals(array[i]);
-                System.out.println(check);
                 if (check) {
                     array[i] = "";
                 }
             }
-            System.out.println(Arrays.toString(array));
             if (array[n]!="") {
-                arrayNew = arrayNew + array[n];
+                arrayNew.append(array[n]);
             }
         }
         System.out.println(arrayNew);
     }
 }
-//        for (int i=0;i < array.length-1 && n<array.length-1;i=i+1) {
-//            boolean check=array[i].equals(array[n+i]);
-//            System.out.println(check);
-//            if (check) {
-//                array[i+1] = "";
-//             } else n=n+1;
-//        }
-            //if (array[n]!="") {
-            //  arrayNew = arrayNew + array[i];
-            //}
+                //arr[0] = "Мой";
+                //arr[1] = "Массив";
+                //arr[2] = "Мой";
+                //arr[3] = "Массив";
+                //arr[4] = "Очень";
+                //arr[5] = "Длинный";
+                //arr[6] = "Очень";
+                //arr[7] = "Длинный";
